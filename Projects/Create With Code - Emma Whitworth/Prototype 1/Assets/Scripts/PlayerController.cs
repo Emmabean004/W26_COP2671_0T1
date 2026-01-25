b6cf5406275104cbbe763aee5f89ed515cd9d21b
Emmabean004 <44015549@sfcollege.edu>
2026-01-25 15:24:30 -0500

    Added Unity Project

---
+++ Projects/Create With Code - Emma Whitworth/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float turnSpeed = 20.0f;
+    private float speed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
